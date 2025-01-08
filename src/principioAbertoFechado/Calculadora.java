017c86d8cc614e66f19f7ce99893e4286b9620a4
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-01-08 17:15:49 -0300

    refactor(calculadora): refatora lógica de cálculo para seguir o princípio aberto/fechado - Substitui a lógica condicional por um registro dinâmico de operações. - Adiciona suporte para registrar novas operações usando um Map. - Implementa validação para operações não registradas. Essa mudança melhora a extensibilidade do código, permitindo adicionar novas operações sem modificar a classe principal.

--- src/principioAbertoFechado/Calculadora.java
+++ src/principioAbertoFechado/Calculadora.java
@@ -1,12 +1,23 @@
 package principioAbertoFechado;
 
+import java.util.HashMap; //implementação concreta da interface Map que usa tabelas hash para armazenar os dados.
+import java.util.Map; //É uma interface que define o comportamento de coleções baseadas em pares de chave-valor.
+
 public class Calculadora {
-    public int calcular(int a, int b, String operacao) {
-        if (operacao.equals("soma")) {
-            return a + b;
-        } else if (operacao.equals("subtracao")) {
-            return a - b;
-        }
-        return 0;
+    private Map<String, Operacao> operacoesRegistradas = new HashMap<>();
+
+    //Método para registrar novas operações
+    public void registrarOperacao (String nomeOperacao, Operacao operacao){
+        operacoesRegistradas.put(nomeOperacao, operacao);
     }
+
+    // Método para calcular usando a operação registrada
+    public int executarOperacao (String nomeOperacao, int numero1, int numero2){
+        Operacao operacao = operacoesRegistradas.get(nomeOperacao);
+        if (operacao == null) {
+            throw new IllegalArgumentException("Operação não registrada: " + nomeOperacao);
+        }
+        return operacao.calcular(numero1, numero1);
+    }
+
 }
