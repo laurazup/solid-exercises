b9bcf91998b45be0af5d56d4c0835e265da822f3
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-01-09 14:21:35 -0300

    feat(class): adicionar classe Desenvolvedor que implementa a interface Programador

---
+++ src/principioSegregacaoInterface/Desenvolvedor.java
@@ -0,0 +1,8 @@
+package principioSegregacaoInterface;
+
+public class Desenvolvedor implements Programador {
+    @Override
+    public void programar() {
+        System.out.println("Programando...");
+    }
+}
