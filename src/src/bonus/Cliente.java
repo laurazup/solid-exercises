b5a55dc8f01d81819ab4668bc1c9c7997033375d
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-15 12:19:31 -0300

    Refactor: Alterações nas validações e no tipo primitivo para LocalDate

--- src/src/bonus/Cliente.java
+++ src/src/bonus/Cliente.java
@@ -1,28 +1,25 @@
 package bonus;
 
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Cliente {
-
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     private String nome;
     private String cpf;
     private String rg;
     private LocalDate dataContaCriada;
 
-    public Cliente(String nome, String cpf, String rg, String dataContaCriada) {
-        verificacao(nome, cpf, rg, LocalDate.parse(dataContaCriada));
+    public Cliente(String nome, String cpf, String rg, LocalDate dataContaCriada) {
+        verificacao(nome, cpf, rg);
         this.nome = nome;
         this.cpf = cpf;
         this.rg = rg;
-        this.dataContaCriada = LocalDate.parse(dataContaCriada, DATE_TIME_FORMATTER);
+        this.dataContaCriada = dataContaCriada;
     }
 
 
-    private void verificacao(String nome, String cpf, String rg, LocalDate dataContaCriada){
+    private void verificacao(String nome, String cpf, String rg){
         if (nome == null || nome.isEmpty()){
             throw new IllegalArgumentException("O nome não pode ser nulo ou vazio!");
         }
@@ -32,9 +29,40 @@ public class Cliente {
         if (rg == null || rg.isEmpty()){
             throw new IllegalArgumentException("O RG não pode ser nulo ou vazio!");
         }
-        if (dataContaCriada == null){
-            throw new IllegalArgumentException("A Data da Criação não pode ser nula!");
-        }
 
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public LocalDate getDataContaCriada() {
+        return dataContaCriada;
+    }
+
+    public void setDataContaCriada(LocalDate dataContaCriada) {
+        this.dataContaCriada = dataContaCriada;
+    }
+
+
 }
